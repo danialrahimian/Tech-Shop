df35909c7ba83e1eca9b4e678c19683fc9ec8938
danialrahimian <onedaniyel@gmail.com>
2025-09-30 16:23:28 +0330

    add provider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
+import store from "./Redux/store.ts";
+
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
